94841d97bf466e7e1fc4bdad3e8556fdb8e66a40
DESKTOP-PSK585N\Sebastian <seb.campos03@gmail.com>
2022-05-09 19:59:31 -0400

    few error fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import logo from '../logo.jpg'
+// import logo from '../logo.jpg'
 const Navbar = () => {
     return (
+        // <BrowserRouter>
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
      <div className="container">
-    <a className="navbar-brand" href="/"><img src={logo} alt="logo..."/></a>
+    {/* <a className="navbar-brand" href="/"><img src={logo} alt="logo..."/></a> */}
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
